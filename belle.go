8517f3c51d6fa53219acd391ef169d6a734560dd
KisuraWSP <kisura2003@gmail.com>
2023-12-27 09:09:26 +0530

    working on fix for repl and docrepl flag

--- belle.go
+++ belle.go
@@ -6,66 +6,6 @@ import (
 	"os"
 )
 
-/*
-# help guideline
-("how we going to structure the `help` flag when called in the program")
-("based on go help")
-
-	Go is a tool for managing Go source code.
-
-	Usage:
-
-        	go <command> [arguments]
-
-	The commands are:
-
-        	bug         start a bug report
-        	build       compile packages and dependencies
-       		clean       remove object files and cached files
-        	doc         show documentation for package or symbol
-		env         print Go environment information
-        	fix         update packages to use new APIs
-        	fmt         gofmt (reformat) package sources
-        	generate    generate Go files by processing source
-        	get         add dependencies to current module and install them
-        	install     compile and install packages and dependencies
-        	list        list packages or modules
-        	mod         module maintenance
-        	work        workspace maintenance
-        	run         compile and run Go program
-        	test        test packages
-        	tool        run specified go tool
-        	version     print Go version
-        	vet         report likely mistakes in packages
-
-	Use "go help <command>" for more information about a command.
-
-	Additional help topics:
-
-        	buildconstraint build constraints
-        	buildmode       build modes
-        	c               calling between Go and C
-        	cache           build and test caching
-       		environment     environment variables
-        	filetype        file types
-        	go.mod          the go.mod file
-        	gopath          GOPATH environment variable
-        	gopath-get      legacy GOPATH go get
-        	goproxy         module proxy protocol
-        	importpath      import path syntax
-        	modules         modules, module versions, and more
-        	module-get      module-aware go get
-        	module-auth     module authentication using go.sum
-        	packages        package lists and patterns
-        	private         configuration for downloading non-public code
-        	testflag        testing flags
-        	testfunc        testing functions
-        	vcs             controlling version control with GOVCS
-
-	Use "go help <topic>" for more information about that topic.
-
-*/
-
 // version 
 var VERSION_NO string = fmt.Sprintf("%v",0.1)
 var VERSION string = "Belle Version " + VERSION_NO
@@ -84,7 +24,7 @@ Usage:
 		repl 		loads the READ-EVAL-PRINT-LOOP
 		fread 		reads the .belle file if given as an argument
 		values 		displays the values for the enums in the language
-		
+			
 `
 
 // reserved keywords and symbols
@@ -211,7 +151,7 @@ func print_reserved_values(){
 	var length int = len(reserved)
 	fmt.Println("VALUES {RESERVED}")
 	for i:=0; i<length; i++ {
-		fmt.Println(i)
+		fmt.Println(reserved[i])
 	}
 }
 
@@ -219,7 +159,7 @@ func print_symbol_values(){
 	var length int = len(symbols)
 	fmt.Println("VALUES {SYMBOLS}")
 	for i:=0; i<length; i++ {
-		fmt.Println(i)
+		fmt.Println(symbols[i])
 	}
 }
 
@@ -227,19 +167,20 @@ func print_primitive_values(){
 	var length int = len(primitives)
 	fmt.Println("VALUES {PRIMITIVES}")
 	for i:=0; i<length; i++ {
-		fmt.Println(i)
+		fmt.Println(primitives[i])
 	}
 }
 
 // documentation
-func docrepl_load() int {
+func docrepl_load() {
+	var state int
 	var input string 
 	fmt.Print("DOC=>")
+	state = 1
 	fmt.Scan(&input)
 	for input != "exit" {
 		if input == "exit" {
 			log.Println("Exiting DOC REPL")
-			break
 		} else if input == "test" {
 			content, err := os.ReadFile("test.txt")
 			if err != nil {
@@ -250,11 +191,17 @@ func docrepl_load() int {
 			break
 		} else {
 			fmt.Println("Invalid Argument Entered!")
+			state = 0
+			break
+		}
+	}
+	if state != 1 {
+		fmt.Print("Do You want to enter again (Y/n): ")
+		fmt.Scan(&input)
+		if input == "y" || input == "Y"{
 			docrepl_load()
 		}
-
 	}
-	return 1
 }
 
 func  doc_handler(file string) {
@@ -318,52 +265,58 @@ func generate_expression(tokenlist []*token){
 
 
 // REPL (READ-EVALUATE-PRINT-LOOP)
-func repl_load() int {
-	var input string
+func repl_load() {
+	var state int
+	var input string 
 	fmt.Print("RUN=>")
+	state = 1
 	fmt.Scan(&input)
 	for input != "exit" {
-		if input == "exit"{
-			log.Print("Exiting Out of REPL")
+		if input == "exit" {
+			log.Println("Exiting REPL")
 			break
-		}else if input == "version"{
+		} else if input == "version" {
 			fmt.Printf("%s\n",VERSION)
+			break
+		} else {
+			fmt.Println("Invalid Argument Entered!")
+			state = 0
+			break
+		}
+		repl_load()
+	}
+	if state != 1 {
+		fmt.Print("Do You want to enter again (Y/n): ")
+		fmt.Scan(&input)
+		if input == "y" || input == "Y"{
 			repl_load()
 		}
 	}
-	return 1
 }
 
 func main() {
-	var run_flag int
 	args := os.Args
 	for i := 1; i < len(args); i++{
-		if args[i] == "run" {
-			// run the source code
-		}else if args[i] == "version" {
-			fmt.Printf("%s\n",VERSION)
-			break
-		}else if args[i] == "doc" {
-			ret := docrepl_load()
-			run_flag = ret
-			break
-		}else if args[i] == "repl" {
-			ret := repl_load()
-			run_flag = ret
-			break
-		}else if args[i] == "fread" {
-			// read the .belle file
-			break
-		}else if args[i] == "values" {
-			print_reserved_values()
-			print_primitive_values()
-			print_symbol_values()
-			break
-		}else {
-			log.Println("Not an valid  argument")
+		switch args[i]{
+			case "run":
+				fmt.Println("TO BE IMPLEMENTED")		
+			case "version":
+				fmt.Printf("%s\n",VERSION)
+			case "doc":
+				docrepl_load()
+			case "repl":
+				repl_load()
+			case "values":
+				print_reserved_values()
+				print_primitive_values()
+				print_symbol_values()
+			case "fread":
+				fmt.Println("TO BE IMPLEMENTED")
+			case "help":
+				fmt.Printf("%s\n",HELP)		
+			default:
+				log.Println("Not an valid  argument")	
+			
 		}
-	}
-	if run_flag != 1 {
-		fmt.Printf("%s",HELP)
 	}
 }
